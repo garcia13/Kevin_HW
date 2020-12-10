7043a4574b3b81a9a6e48bda90d1904f0dd7b8eb
Miguel Garcia <miguelgarcia@pursuit.org>
2020-12-10 00:13:59 -0500

    Intro To Networking Ted Talk complete

---
+++ introToNetworkingTedTalk.js
@@ -0,0 +1,42 @@
+// 10 WAYS TO HAVE A BETTER CONVERSATION
+//      1. Don't multitask.
+//      2. Don't pontificate.
+//      3. Use open-ended questions.
+//      4. Go with the flow.
+//      5. If you don't know, say that you don't know.
+//      6. Don't equate your experience with theirs.
+//      7. Try not to repeat yourself.
+//      8. Stay out of the weeds.
+//      9. Listen
+//      10. Be brief.
+// 
+// 
+// Which of the 10 Ways to have a Better Conversation do you already practice and feel confident in?
+//      I already feel confident and practice number 3, use open ended questions.
+// 
+// 
+// How can you incorporate this as you network?
+//      When I meet someone I can network with or perhaps gain some useful information from,
+//      I can ask them an open ended question in order to keep the converation going.
+//      If the conversation goes well, perhaps I am able to connect with them.
+// 
+// 
+// Which of these 10 Ways to have a Better Conversation do you think you have space to improve on?
+//      I could improve on number 10, be brief.
+// 
+// 
+// How do you think you can improve on this in your everyday conversation?
+//      I am not sure how to improve on this because sometimes, I feel like I have
+//      to carry the coversation. Some people are not as talkative and I guess I
+//      wanna keep it going until I see an out.
+// 
+// 
+// From your perspective, what are the key take aways from this TedTalk?
+//      "A conversation requires a balance between talking and listening..."
+//      "There is no reason to learn how to show you're paying attention if you are in fact paying attention."
+// 
+// 
+// Anything you find surprising or disagree with?
+//      I think I agree with everything Celeste Headlee was saying in her presentation.
+//      From her intro where she speaks about society to when she seemlessly segues into her
+//      10 rules.
